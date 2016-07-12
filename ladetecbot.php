c5585604eead6ddaa4c0dfc39d3f35f06f3233f6
Daniel Furtado <daniel.96.furtado@gmail.com>
2016-07-12 13:34:37 -0300

    sqlite database
    The bot is using a sqlite3 temp table to store the request.

--- ladetecbot.php
+++ ladetecbot.php
@@ -3,24 +3,18 @@
 define('BOT_TOKEN', '191671065:AAFMbIGQgsY2V2td099yt9I9HzCC6cl6t7Y');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
-function apiRequestWebhook($method, $parameters) {
-  if (!is_string($method)) {
-    error_log("Method name must be a string\n");
-    return false;
-  }
+function sqlite_open($location)
+{
+    $handle = new SQLite3($location);
+    return $handle;
+}
 
-  if (!$parameters) {
-    $parameters = array();
-  } else if (!is_array($parameters)) {
-    error_log("Parameters must be an array\n");
-    return false;
-  }
-
-  $parameters["method"] = $method;
-
-  header("Content-Type: application/json");
-  echo json_encode($parameters);
-  return true;
+function sqlite_query($dbhandle,$query)
+{
+    $array['dbhandle'] = $dbhandle;
+    $array['query'] = $query;
+    $result = $dbhandle->query($query);
+    return $result;
 }
 
 function exec_curl_request($handle) {
@@ -115,50 +109,114 @@ function apiRequestJson($method, $parameters) {
 
 function processMessage($message) {
   // process incoming message
-  $message_id = $message['message_id'];
   $chat_id = $message['chat']['id'];
   if (isset($message['text'])) {
     // incoming text message
     $text = $message['text'];
 
-    if ($text === "/start") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Qual seu problema?', 'reply_markup' => array(
-        'keyboard' => array(array('Banco de Dados'), array ('Equipamentos de Informática ou Programas'), array ('Outros')),
-        'one_time_keyboard' => true,
-        'resize_keyboard' => true)));
-    } else if ($text === "Banco de Dados") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Em qual banco esta tendo problemas?', 'reply_markup' => array(
-        'keyboard' => array(array('Lims'), array ('Soluções'), array ('LBCD'), array ('Outro Banco de Dados')),
-        'one_time_keyboard' => true,
-        'resize_keyboard' => true)));
-    } else if ($text === "Equipamentos de Informática ou Programas") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Qual tipo de equipamento?', 'reply_markup' => array(
-        'keyboard' => array(array('Computador ou Periférico'), array('Impressora'),array('Controle de acesso'),array('Outro Equipamento')),
-        'one_time_keyboard' => true,
-        'resize_keyboard' => true)));
-    } else if ($text === "Outros") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Qual seu problema?', 'reply_markup' => array(
-        'keybaord' => array(array('VOIP'), array('Rede'),array('Outro')),
-        'one_time_keyboard' => true,
-        'resize_keyboard' => true)));
-    }else if ($text === "Lims" || $text === "Soluções" || $text === "LBCD" || $text === "Rede") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Descreva seu problema:'));
-    }else if ($text === "Outro Banco de Dados") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Descreva seu problema e o banco:'));
-    }else if ($text === "Outro Equipamento") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Descreva seu problema e o equipamento:'));
-    }else if ($text === "Outro") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Onde se encontra o problema?'));
-    }else if ($text === "Computador ou Periférico" || $text === "Impressora" || $text === "Controle de acesso" || $text === "VOIP") {
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Onde se encontra o equipamento?'));
-    }else if (false) {// tratar esse caso qnd o canco estiver pronto
-      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Descreva seu problema:'));
-    }else {
-      apiRequestWebhook("sendMessage", array('chat_id' => $chat_id, "text" => 'A equipe de TI estrá empenhada em resolver seu problema.'));
-    }
-    }
-    else {
-    apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Diga /start para começar, e selecione nossas opções.'));
+      switch ($text){
+        case "/start": 
+              apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Qual seu problema?', 'reply_markup' => array(
+              'keyboard' => array(array('Banco de Dados'), array ('Equipamentos de Informática ou Programas'), array ('Outros')),
+              'one_time_keyboard' => true,
+              'resize_keyboard' => true)));
+              
+              $dbhandle=sqlite_open("ladetecbot.db");
+              $return=sqlite_query($dbhandle,
+                "Delete from temp where id='".$chat_id."';"
+              );
+              $return=sqlite_query($dbhandle,
+                "Insert into temp values (".$chat_id.", '', '');"
+              );break;
+      case "Banco de Dados":
+              apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Em qual banco esta tendo problemas?', 'reply_markup' => array(
+              'keyboard' => array(array('Lims'), array ('Soluções'), array ('LBCD'), array ('Outro Banco de Dados')),
+              'one_time_keyboard' => true,
+              'resize_keyboard' => true)));break;
+      case "Equipamentos de Informática ou Programas":
+              apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Qual tipo de equipamento?', 'reply_markup' => array(
+              'keyboard' => array(array('Computador ou Periférico'), array('Impressora'),array('Controle de acesso'),array('Outro Equipamento')),
+              'one_time_keyboard' => true,
+              'resize_keyboard' => true)));break;
+      case "Outros":
+              apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Qual seu problema?', 'reply_markup' => array(
+              'keyboard' => array(array('VOIP'), array('Rede'),array('Outro')),
+              'one_time_keyboard' => true,
+              'resize_keyboard' => true)));break;
+      case "Rede":
+              apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Qual seu problema?', 'reply_markup' => array(
+              'keyboard' => array(array('Acesso no Sigel'), array('Acesso a Rede'),array('Outro Problema de Rede')),
+              'one_time_keyboard' => true,
+              'resize_keyboard' => true)));
+      case "Outro Banco de Dados":
+            apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Descreva seu problema e o banco:'));
+            
+            $dbhandle=sqlite_open("ladetecbot.db");
+              $return=sqlite_query($dbhandle,
+                "Update temp set assunto='".$text."', local='LBCD' where id='".$chat_id."';"
+              );  
+      case "Lims":
+      case "Soluções":
+      case "LBCD":
+      case "Rede":          
+      case "Acesso a Rede": 
+      case "Acesso no Sigel":
+            apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Descreva seu problema e o local caso necessário:'));
+            
+            $dbhandle=sqlite_open("ladetecbot.db");
+              $return=sqlite_query($dbhandle,
+                "Update temp set assunto='".$text."', local='LBCD' where id='".$chat_id."';"
+              );break;
+      case "Outro" :
+      case "Outro Problema de Rede":        
+      case "Outro Equipamento":        
+      case "Computador ou Periférico" :
+      case "Impressora":        
+      case "Controle de acesso":
+      case "VOIP":  
+            apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Qual o equipamento? E onde ele se encontra?'));
+            $dbhandle=sqlite_open("ladetecbot.db");
+              $return=sqlite_query($dbhandle,
+                "Update temp set assunto='".$text."' where id='".$chat_id."';"
+              );break;         
+      default:    
+            $dbhandle=sqlite_open("ladetecbot.db");
+            $return=sqlite_query($dbhandle,"Select * from temp where id='".$chat_id."';");
+            $resx = $return->fetchArray();
+            
+            if($resx[1]===''){
+              apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Assunto inválido!', 'reply_markup' => array(
+              'keyboard' => array(array('Banco de Dados'), array ('Equipamentos de Informática ou Programas'), array ('Outros')),
+              'one_time_keyboard' => true,
+              'resize_keyboard' => true)));
+            }
+            else if($resx[2]===''){
+              apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Descreva seu problema:'));
+              $result=sqlite_query($dbhandle,
+                "Update temp set local='".$text."' where id='".$chat_id."';"
+                );
+            }
+            else {
+              apiRequestJson("sendMessage", array('chat_id' => $chat_id , "text" => 'A equipe de TI estrá empenhada em resolver seu problema.'));
+              
+              /*
+              apiRequestJson("sendMessage", array('chat_id' => '-116137583' , "text" => $message['from']['first_name'].' '.$message['from']['last_name']));
+              apiRequestJson("sendMessage", array('chat_id' => '-116137583' , "text" => 'Assunto: '.$resx[1]));
+              apiRequestJson("sendMessage", array('chat_id' => '-116137583' , "text" => 'Local: '.$resx[2]));
+              apiRequestJson("sendMessage", array('chat_id' => '-116137583' , "text" => 'Problema: '.$text));
+              */
+              
+              /*
+              adicionar banco helpdesk
+              */
+              
+              $return=sqlite_query($dbhandle,
+                "Delete from temp where id='".$chat_id."';"
+              );
+              
+            }
+        }
+    $dbhandle->close();
   }
 }
 
@@ -169,5 +227,5 @@ $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
 if (isset($update["message"])) {
-  processMessage($update["message"]);
+  processMessage($update["message"],$update["user"]);
 }
